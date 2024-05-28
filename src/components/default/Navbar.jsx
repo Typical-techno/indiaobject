6af7da3170f97c310a0ee91f0b60e4c1e265a327
typical-techno <shubhamjangrartk@gmail.com>
2024-05-28 14:32:39 +0530

    added tweaks in navbar

--- src/components/default/Navbar.jsx
+++ src/components/default/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
   ];
 
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 py-2">
       <div className="flex items-center justify-center">
         <div className="text-5xl font-bold">
           <Link to="/">
